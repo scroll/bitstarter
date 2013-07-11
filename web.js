10b2846fde1c3484dab3c9c689b5dfb44e53ecf3
Marin Petrov <scroll_lock@abv.bg>
2013-07-11 05:18:45 +0000

    Adding html file.

--- web.js
+++ web.js
@@ -2,11 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+require('buffer');
+var fs = require('fs');
+
+var buf = new Buffer(fs.readFileSync("index.html"));
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(buf.toString("utf-8"))
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
+
